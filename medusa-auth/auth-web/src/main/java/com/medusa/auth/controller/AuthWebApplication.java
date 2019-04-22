32738666a51b67f8ce3b7130a5f1533abe707cae
daitu <daitu@bxjjren.com>
2019-04-22 18:47:41 +0800

    基本基调

---
+++ medusa-auth/auth-web/src/main/java/com/medusa/auth/controller/AuthWebApplication.java
@@ -0,0 +1,15 @@
+package com.medusa.auth.controller;
+
+/**
+ * @program: medusa
+ * @description: 项目启动类
+ * @author: PowerZzzD
+ * @date: 2019/04/16 10:27
+ */
+
+public class AuthWebApplication {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ medusa-auth/auth-web/src/main/java/com/medusa/auth/controller/login/LoginController.java
@@ -0,0 +1,15 @@
+package com.medusa.auth.controller.login;
+
+/**
+ * @program: medusa
+ * @description: 登陆接口
+ * @author: PowerZzzD
+ * @date: 2019/04/16 10:12
+ */
+
+
+public class LoginController {
+
+
+
+}
