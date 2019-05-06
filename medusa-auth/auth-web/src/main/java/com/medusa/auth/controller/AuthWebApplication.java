c9501ea5c1577b50598186b0028071acc861e362
daitu <daitu@bxjjren.com>
2019-05-06 14:16:44 +0800

    基本基调

--- medusa-auth/auth-web/src/main/java/com/medusa/auth/controller/AuthWebApplication.java
+++ medusa-auth/auth-web/src/main/java/com/medusa/auth/controller/AuthWebApplication.java
@@ -1,15 +1,21 @@
 package com.medusa.auth.controller;
 
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
 /**
  * @program: medusa
- * @description: 项目启动类
+ * @description: 账户项目启动类
  * @author: PowerZzzD
  * @date: 2019/04/16 10:27
  */
-
+@SpringBootApplication
 public class AuthWebApplication {
 
     public static void main(String[] args) {
-
+        new SpringApplicationBuilder(AuthWebApplication.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
     }
 }
--- medusa-auth/auth-web/src/main/java/com/medusa/auth/controller/login/LoginController.java
+++ medusa-auth/auth-web/src/main/java/com/medusa/auth/controller/login/LoginController.java
@@ -1,14 +1,18 @@
 package com.medusa.auth.controller.login;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @program: medusa
  * @description: 登陆接口
  * @author: PowerZzzD
  * @date: 2019/04/16 10:12
  */
-
-
+@RestController
+@RequestMapping(value = "/login")
 public class LoginController {
+
 
 
 
