2d7fabd3c3048bf141aa99fd1f3b2e09b61b3bf5
PowerZzzD <zhangdi_power@163.com>
2020-05-29 16:35:17 +0800

    调整maven子父项目版本管理引入

--- medusa-gateway/src/main/java/com/medusa/gateway/GateWayApplication.java
+++ medusa-gateway/src/main/java/com/medusa/gateway/GateWayApplication.java
@@ -2,21 +2,15 @@ package com.medusa.gateway;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaClient
 public class GateWayApplication {
 
     public static void main(String[] args) {
-        //这样写即可 默认非web响应
         SpringApplication.run(GateWayApplication.class, args);
-//        new SpringApplicationBuilder(GateWayApplication.class)
-//                .web(WebApplicationType.REACTIVE)
-//                .run(args);
     }
 
 }
